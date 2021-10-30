aef611d481b946391e4e0df0384557b1d9529a72
YoniLeulseged <leulsegedyonatan@gmail.com>
2021-10-30 21:42:54 +0300

    initial_commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.visibility = "visible";
+    // navLinks.style.position = "fixed";
+
+
+}
+function hideMenu() {
+    navLinks.style.visibility = "hidden";
+    navLinks.style.transition = "visibility .4s";
+
+
+
+
+}
+
